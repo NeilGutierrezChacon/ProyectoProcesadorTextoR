5bafe654aa00fa22f0f84506b37de056b4de6ec7
Neil <n.g.ch43@gmail.com>
2018-02-24 01:06:32 +0100

    Arradimos un menu al procesador de texto

--- src/procesadorTexto/LaminaProcesadorTexto.java
+++ src/procesadorTexto/LaminaProcesadorTexto.java
@@ -1,7 +1,8 @@
 package procesadorTexto;
 
 import java.awt.BorderLayout;
-
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
@@ -19,9 +20,44 @@ public class LaminaProcesadorTexto extends JPanel {
 		
 		setLayout(new BorderLayout());//establecemos un Layout de tipo Border
 		
+		JPanel laminaMenuPrincipal = new JPanel();//Lamina donde estableceremos el menu
+		
+		JMenuBar barraPrincipal=new JMenuBar();//instancia un menuBarra para el menu principal
+		
 		hoja=new JTextArea();//instancia de la variable hoja como JTextArea
 		
 		add(hoja,BorderLayout.CENTER);//añadiendo la hoja en la parte norte
+		//--------------------------------------------------------------------------//
+		JMenu archivo =new JMenu("Archivo");
+		
+		JMenu edicion =new JMenu("Edicion");
+		
+		JMenu formato =new JMenu("Formato");
+		
+		JMenu ver =new JMenu("Ver");
+		
+		JMenu ayuda =new JMenu("Ayuda");
+		
+		barraPrincipal.add(archivo);
+		
+		barraPrincipal.add(edicion);
+		
+		barraPrincipal.add(formato);
+		
+		barraPrincipal.add(ver);
+		
+		barraPrincipal.add(ayuda);
+		
+		
+		
+		
+		
+		laminaMenuPrincipal.add(barraPrincipal);//añadimos la barra principal a la lamina
+		//del menu principal NECESARIO AÑADIRLOS AL FINAL 
+		
+		add(laminaMenuPrincipal,BorderLayout.NORTH);//añadimos la lamina de menus a la lamina 
+		//principal en la posicion norte 
+		
 		
 		
 	}
